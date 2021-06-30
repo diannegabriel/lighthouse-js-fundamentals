bd5f5b325181b9ad71f45098861a1592ec7dc02e
Dianne <mdianne.gabriel@gmail.com>
2021-06-30 10:25:36 -0400

    Added comments on odd.js for clarification

--- odd.js
+++ odd.js
@@ -1,3 +1,12 @@
+/*
+Challenge
+Create a function to determine if a number is odd or not.
+
+Create a new file in your lighthouse-js-fundamentals directory in Vagrant called odd.js.
+Inside the file, define a function called isOdd.
+This function should take a number as a parameter and return true if it is odd and false otherwise.
+*/
+
 function isOdd (num) {
   return num % 2 === 1
 }
